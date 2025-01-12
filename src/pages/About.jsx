27a9a9bc2afcc76bb754ea41592e3efe29ab80a9
ozgur <95157036+vasterim@users.noreply.github.com>
2025-01-12 23:36:59 +0300

    add about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,25 @@
 function About() {
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold">About Page</h1>
+    <div className="container mx-auto p-6 lg:px-32">
+      <div className="bg-gray-100 p-6 rounded-lg shadow-md">
+        <h1 className="text-3xl font-bold mb-4 text-gray-800">Hakkımızda</h1>
+        <p className="text-lg font-light text-gray-700 mb-4">
+          Firmamız, müşteri memnuniyetini ön planda tutarak, kaliteli ürün ve
+          hizmet sunmayı misyon edinmiştir. Teknolojiyi takip ederek modern
+          çözümler sunar ve topluma değer katmayı hedefleriz.
+        </p>
+        <h2 className="text-2xl font-semibold text-gray-800 mb-3">Misyonumuz</h2>
+        <p className="text-base text-gray-700 mb-4">
+          Müşterilerimize en iyi deneyimi sağlamak ve sürekli olarak gelişen
+          ürünler sunmak için çalışıyoruz. Her adımda çevre dostu ve sürdürülebilir
+          çözümler sunmayı ilke ediniyoruz.
+        </p>
+        <h2 className="text-2xl font-semibold text-gray-800 mb-3">Vizyonumuz</h2>
+        <p className="text-base text-gray-700">
+          Gelecekte sektörde öncü bir marka olmak ve tüm müşterilerimiz için
+          güvenilir bir iş ortağı haline gelmek.
+        </p>
+      </div>
     </div>
   );
 }
